1727e769b2880aa47c75c408e69855e21593341f
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-08-21 12:40:16 +0300

    redux -> contacts-selector -> const getVisibleContacts -> with "createSelector" method

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -1,12 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const getContact = state => state.contacts.items;
 export const getFilter = state => state.contacts.filter;
 
-export const getVisibleContacts = state => {
-  const contacts = getContact(state);
-  const filter = getFilter(state);
-  const normalizedFilter = filter.toLowerCase();
+export const getVisibleContacts = createSelector(
+  [getContact, getFilter],
+  (items, filter) => {
+    const filteredItems = items.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    );
 
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(normalizedFilter),
-  );
-};
+    return filteredItems;
+  }
+);
